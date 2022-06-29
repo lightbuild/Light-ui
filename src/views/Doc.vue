928980d6e30498709c3767c47cec6c03a94760ba
lightbuild <renran6303@gmail.com>
2022-06-29 14:48:51 +0800

    初始化aside的html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,17 +2,42 @@
   <div>
     <Topnav/>
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch`">
+              Switch组件
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button`">
+              Button组件
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog`">
+              Dialog组件
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs`">
+              Tabs组件
+            </router-link>
+          </li>
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import Topnav from '../components/Topnav.vue'
+  import Topnav from '../components/Topnav.vue';
+  
   export default {
     components: {Topnav}
-  }
+  };
 </script>
 
 <style lang="scss" scoped>
