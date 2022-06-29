e70c02c03300847ce33e40b654c6a0ddac71195f
lightbuild <renran6303@gmail.com>
2022-06-29 14:37:48 +0800

    topnav组件封装

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>导航栏|
-    <router-link to="/">Light</router-link>
+    <router-link to="/">菜单1</router-link>
     |
-    <router-link to="/doc">Light2</router-link>
+    <router-link to="/doc">菜单2</router-link>
   </div>
   <hr/>
   <router-view/>
