613adfec8df791504bb2681d64cfeb636c7f687d
lightbuild <renran6303@gmail.com>
2022-06-29 11:10:10 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hi</div>
+  <light></light>
+</template>
+
+<script>
+import light from './components/light.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    light
+  }
+}
+</script>
