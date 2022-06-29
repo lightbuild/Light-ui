e593a7684d9dc06600469e18e10c5482de4fa683
lightbuild <renran6303@gmail.com>
2022-06-29 14:43:38 +0800

    home组件样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>Light UI</h1>
       <h2>一个厉害的UI框架</h2>
       <p class="actions">
-        <a>GitH ub</a>
-        <a>开始</a>
+        <a href="https://github.com/">GitH ub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -21,4 +21,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+      padding: 8px 0;
+      a {
+        margin:0 8px;
+        background: #fff;
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+      }
+    }
+  }
 </style>
