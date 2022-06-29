a50cf4d2cd7e290822d7f3e86868a7c22ce6102c
lightbuild <renran6303@gmail.com>
2022-06-29 17:25:12 +0800

    优化整体css效果

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -28,10 +28,13 @@
     background: pink;
     display: flex;
     padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 10;
     justify-content: center;
     align-items: center;
-    position: relative;
-    z-index: 10;
     > .logo {
       max-width: 6em;
       margin-right: auto;
@@ -44,18 +47,25 @@
         margin: 0 1em;
       }
     }
+    > .toggleAside {
+      width: 24px;
+      height: 24px;
+      background: red;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
+      display: none;
+    }
     @media (max-width: 500px) {
-      > .menu{display: none;}
-      > .logo{margin:0 auto}
-      > .toggleAside{
+      > .menu {
+        display: none;
+      }
+      > .logo {
+        margin: 0 auto;
+      }
+      > .toggleAside {
         display: inline-block;
-        width: 24px;
-        height: 24px;
-        background:red;
-        position: absolute;
-        left:16px;
-        top:50%;
-        transform: translateY(-50%);
       }
     }
   }
