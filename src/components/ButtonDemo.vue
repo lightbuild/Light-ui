f0243232b14fe16f26fd49ef40650c2da008c3d4
lightbuild <renran6303@gmail.com>
2022-06-30 14:29:37 +0800

    创建Button组件

---
+++ src/components/ButtonDemo.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>示例1</p>
+  <Button>你好</Button>
+</template>
+
+<script lang="ts">
+  import Button from '../lib/Button.vue'
+  export default {
+    components:{Button}
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import {createRouter, createWebHashHistory} from 'vue-router';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 import Switch from './components/SwitchDemo.vue';
-import Button from './components/Button.vue';
+import Button from './components/ButtonDemo.vue';
 import Dialog from './components/Dialog.vue';
 import Tabs from './components/Tabs.vue';
 import DocDemo from './components/DocDemo.vue';
