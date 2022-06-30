9d60dd69c64b01ad2ab24d3ad83edc5a047ec8ac
lightbuild <renran6303@gmail.com>
2022-06-30 14:21:50 +0800

    优化switch css代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -4,18 +4,18 @@
 
 <script lang="ts">
   import {ref} from 'vue';
-
-  export default{
-    props:{
-      value:Boolean
+  
+  export default {
+    props: {
+      value: Boolean
     },
-    setup(props,context){
-      const toggle = ()=>{
-        context.emit('update:value',!props.value)
-      }
-      return{toggle}
+    setup(props, context) {
+      const toggle = () => {
+        context.emit('update:value', !props.value);
+      };
+      return {toggle};
     }
-  }
+  };
 </script>
 
 <style lang="scss" scoped>
@@ -27,33 +27,43 @@
     border: none;
     background: gray;
     border-radius: $h/2;
-    position: relative
+    position: relative;
+    
+    > span {
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      height: $h2;
+      width: $h2;
+      background: white;
+      border-radius: $h2/2;
+      transition: all 250ms;
+    }
+    
+    &.checked {
+      background: #1890ff;
+      
+      > span {
+        left: calc(100% - #{$h2} - 2px);
+      }
+    }
+    
+    &:active {
+      > span {
+        width: $h2 + 4px;
+      }
+    }
+    
+    &.checked:active {
+      > span {
+        width: $h2 + 4px;
+        margin-left: -4px;
+      }
+    }
+    
+    &:focus {
+      outline: none;
+    }
   }
-  
-  span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background: white;
-    border-radius: $h2/2;
-    transition: all 250ms;
-  }
-  
-  button.checked {
-    background: #1890ff;
-  }
-  button.checked > span {
-    left: calc(100% - #{$h2} - 2px);
-  }
-  button:active{
-    > span {width: $h2 + 4px;}
-  }
-  button.checked:active{
-    > span {width: $h2 + 4px; margin-left: -4px;}
-  }
-  button:focus{
-    outline: none;
-  }
+
 </style>
