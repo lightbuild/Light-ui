627821b21f133b99828dc3f6f9169f719252ba32
lightbuild <renran6303@gmail.com>
2022-07-01 14:04:52 +0800

    Dialog组件支持visible属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,14 +1,25 @@
 <template>
   <div>Dialog</div>
- <h1>示例1</h1>
-  <Dialog></Dialog>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog :visible="visible"></Dialog>
 </template>
 
 <script lang="ts">
-  import Dialog from "../lib/Dialog.vue"
-  export default{
-    components:{Dialog}
-  }
+  import Dialog from '../lib/Dialog.vue';
+  import Button from '../lib/Button.vue';
+  import {ref} from 'vue';
+  
+  export default {
+    setup(){
+      const visible =ref(false);
+      const toggle = ()=>{
+        visible.value = !visible.value;
+      }
+      return{toggle,visible}
+    },
+    components: {Button, Dialog}
+  };
 </script>
 
 <style lang="scss" scoped>
