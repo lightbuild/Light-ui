bf058773a0fdd4d5ab8c63ece68a0a6ff38bd72c
lightbuild <renran6303@gmail.com>
2022-07-02 14:54:41 +0800

    使用:key显示被选中的内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x" >
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
@@ -9,8 +9,13 @@
 <script lang="ts">
   import Tab from '../lib/Tab.vue';
   import Tabs from '../lib/Tabs.vue';
+  import {ref} from 'vue';
   
   export default {
+    setup() {
+      const x = ref('导航1')
+      return{x}
+    },
     components: {Tabs, Tab}
   };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="light-tabs">
     <div class="light-tabs-nav">
-      <div class="light-tabs-nav-item" v-for="(title,index) in titles" :key="index">{{ title }}</div>
+      <div class="light-tabs-nav-item" :class="{selected:title === selected}" @click="select(title)"
+           v-for="(title,index) in titles"
+           :key="index">{{ title }}
+      </div>
     </div>
     <div class="light-tabs-content">
-      <component class="light-tabs-content-item" v-for="c in defaults" :is="c"/>
+      <component class="light-tabs-content-item" :is="current" :key="current.props.title"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
   import Tab from './Tab.vue';
+  import {computed} from 'vue';
   
   export default {
+    props: {
+      selected: {
+        type: String,
+      }
+    },
     setup(props, context) {
       const defaults = context.slots.default();
       defaults.forEach((tab) => {
@@ -20,8 +29,14 @@
           throw new Error('Tabs 的子标签必须是 Tab');
         }
       });
+      const select = (title: string) => {
+        context.emit('update:selected', title);
+      };
+      const current = computed(() => {
+        return defaults.find((tab) => tab.props.title === props.selected);
+      });
       const titles = defaults.map(tab => tab.props.title);
-      return {defaults, titles};
+      return {defaults, titles, current, select};
     }
   };
 </script>
@@ -35,18 +50,22 @@
       display: flex;
       color: $color;
       border-bottom: 1px solid $border-color;
+      
       &-item {
         padding: 8px 0;
         margin: 0 16px;
         cursor: pointer;
+        
         &:first-child {
           margin-left: 0;
         }
+        
         &.selected {
           color: $blue;
         }
       }
     }
+    
     &-content {
       padding: 8px 0;
     }
