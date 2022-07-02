217280166269ae484f5bf08876ea5c0194c4b696
lightbuild <renran6303@gmail.com>
2022-07-02 14:12:25 +0800

    修改Tabs组件css样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <div v-for="(title,index) in titles" :key="index">{{ title }}</div>
-    <component v-for="c in defaults" :is="c"/>
+  <div class="light-tabs">
+    <div class="light-tabs-nav">
+      <div class="light-tabs-nav-item" v-for="(title,index) in titles" :key="index">{{ title }}</div>
+    </div>
+    <div class="light-tabs-content">
+      <component class="light-tabs-content-item" v-for="c in defaults" :is="c"/>
+    </div>
   </div>
 </template>
 
@@ -23,5 +27,28 @@
 </script>
 
 <style lang="scss" scoped>
-
+  $blue: #40a9ff;
+  $color: #333;
+  $border-color: #d9d9d9;
+  .light-tabs {
+    &-nav {
+      display: flex;
+      color: $color;
+      border-bottom: 1px solid $border-color;
+      &-item {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+        &:first-child {
+          margin-left: 0;
+        }
+        &.selected {
+          color: $blue;
+        }
+      }
+    }
+    &-content {
+      padding: 8px 0;
+    }
+  }
 </style>
