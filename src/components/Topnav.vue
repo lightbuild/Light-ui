82f0947ce84a756abd2071e1a29dbf860dd54a74
lightbuild <renran6303@gmail.com>
2022-07-06 16:25:01 +0800

    更新切换按钮的样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,35 +1,45 @@
 <template>
   <div class="topnav">
-    <div class="logo" >
-      <svg class="iconpark-icon"><use href="#king"></use></svg>
+    <div class="logo">
+      <svg class="iconpark-icon">
+        <use href="#king"></use>
+      </svg>
     </div>
     <ul class="menu">
       <li>
         <router-link to="/doc">文档</router-link>
       </li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <svg class="iconpark-icon toggleAside" @click="toggleMenu" v-if="toggleAsideVisible">
+      <use href="#application-menu"></use>
+    </svg>
   </div>
 </template>
 
 <script lang="ts">
   import {inject, Ref} from 'vue';
-
+  
   export default {
-    setup(){
-      const menuVisible = inject<Ref<boolean>>('menuVisible')
-      const toggleMenu =()=>{
-        menuVisible.value = !menuVisible.value
+    props: {
+      toggleAsideVisible: {
+        type: Boolean,
+        default: false,
       }
-      return {toggleMenu}
+    },
+    setup() {
+      const menuVisible = inject<Ref<boolean>>('menuVisible');
+      const toggleMenu = () => {
+        menuVisible.value = !menuVisible.value;
+      };
+      return {toggleMenu};
     }
-  }
+  };
 </script>
 
 <style lang="scss" scoped>
   $color: #007974;
   .topnav {
-    color:$color;
+    color: $color;
     display: flex;
     padding: 16px;
     position: fixed;
@@ -39,32 +49,39 @@
     z-index: 10;
     justify-content: center;
     align-items: center;
+    
     > .logo {
       max-width: 6em;
       margin-right: auto;
-      > svg{
+      
+      > svg {
         width: 32px;
         height: 32px;
       }
     }
+    
     > .menu {
       display: flex;
       white-space: nowrap;
       flex-wrap: nowrap;
+      
       > li {
         margin: 0 1em;
       }
     }
+    
     > .toggleAside {
       width: 24px;
       height: 24px;
-      background: red;
       position: absolute;
       left: 16px;
       top: 50%;
       transform: translateY(-50%);
       display: none;
+      color:black;
+      background: fade-out(black,0.9);
     }
+    
     @media (max-width: 500px) {
       > .menu {
         display: none;
