a1b15a94e11089a86e92fe99d9c1adc70f54973c
lightbuild <renran6303@gmail.com>
2022-07-08 14:59:52 +0800

    高亮源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{ Switch1Demo.__sourceCode }}</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html,'html')"></pre>
       </div>
     </div>
     <div class="demo">
@@ -22,7 +22,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{ Switch2Demo.__sourceCode }}</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html')"></pre>
       </div>
     </div>
   </div>
@@ -34,12 +34,14 @@
   import Button from '../lib/Button.vue';
   import Switch1Demo from './Switch1.Demo.vue';
   import Switch2Demo from './Switch2.Demo.vue';
-  
+  import 'prismjs';
+  import 'prismjs/themes/prism.css'
+  const Prism = window.Prism;
   export default {
     components: {Button, Switch},
     setup() {
       const bool = ref(false);
-      return {bool, Switch1Demo, Switch2Demo};
+      return {bool, Switch1Demo, Switch2Demo,Prism};
     }
   };
 </script>
